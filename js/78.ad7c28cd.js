c79fc2bf99d5e15fb762c606332ed8b673705e78
nss27 <ghtjd8582@gmail.com>
2022-06-20 17:00:44 +0900

    구글 시트 api 연동 및 로고, data null 컴포넌트 추가

---
+++ js/78.ad7c28cd.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkQUAD"]=self["webpackChunkQUAD"]||[]).push([[78],{6078:function(e,t,n){n.r(t),n.d(t,{startStatusTap:function(){return i}});var r=n(65),s=n(8593),o=n(6587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const i=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,r.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,i=document.elementFromPoint(t/2,n/2);if(!i)return;const a=(0,s.a)(i);a&&new Promise((e=>(0,o.c)(a,e))).then((()=>{(0,r.Iu)((async()=>{a.style.setProperty("--overflow","hidden"),await(0,s.s)(a,300),a.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=78.ad7c28cd.js.map
